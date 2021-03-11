6734c2f4a17be74ba4a732e441ab10aac8a68787
Nikita Baranov <nikmacentosh@gmail.com>
2021-03-11 21:02:00 +0200

    Sidebar implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Overview from './pages/Overview';
+import Reports from './pages/Reports';
+import Users from './pages/Users';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Sidebar />
+      <Switch>
+        <Route path="/overview" exact component={Overview} />
+        <Route path="/overview/users" exact component={Users} />
+        <Route path="/reports" exact component={Reports} />
+      </Switch>
+    </Router>
+    
   );
 }
 
---
+++ src/components/SidebarData.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import * as RiIcons from 'react-icons/ri';
+
+export const SidebarData = [
+    {
+        title: 'Overview',
+        path: '/overview',
+        icon: <AiIcons.AiFillHome />,
+        iconClosed: <RiIcons.RiArrowDownSFill />,
+        iconOpened: <RiIcons.RiArrowUpSFill />,
+
+        subNav: [
+            {
+                title: 'Users',
+                path: '/overview/users',
+                icon: <IoIcons.IoIosPaper />,
+            },
+            {
+                title: 'Revenue',
+                path: '/overview/revenue',
+                icon: <IoIcons.IoIosPaper />,
+            },
+        ]
+    },
+    {
+        title: 'Reports',
+        path: '/reports',
+        icon: <AiIcons.AiFillHome />,
+        iconClosed: <RiIcons.RiArrowDownSFill />,
+        iconOpened: <RiIcons.RiArrowUpSFill />,
+        
+        subNav: [
+            {
+                title: 'Report ',
+                path: '/reports/report1',
+                icon: <IoIcons.IoIosPaper />,
+            },
+            {
+                title: 'Report 2',
+                path: '/overview/report2',
+                icon: <IoIcons.IoIosPaper />,
+            },
+            {
+                title: 'Report 3',
+                path: '/overview/report3',
+                icon: <IoIcons.IoIosPaper />,
+            },
+        ]
+    },
+    {
+        title: 'Products',
+        path: '/products',
+        icon: <FaIcons.FaCartPlus />,
+    }
+]
+
+
